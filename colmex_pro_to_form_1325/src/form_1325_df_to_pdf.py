2b2ce1c1a09d5e0b2a398eec78cb21a50c3b0a2b
dorayalon <111888246+dorayalon@users.noreply.github.com>
2024-06-03 20:50:49 +0300

    Update form_1325_df_to_pdf.py
    Added comments and documentation

--- colmex_pro_to_form_1325/src/form_1325_df_to_pdf.py
+++ colmex_pro_to_form_1325/src/form_1325_df_to_pdf.py
@@ -16,7 +16,13 @@ class Form1325DFToPDF:
         self.OUTPUT_PATH = output_path
 
     @staticmethod
-    def round_number(num: float, decimals: int):
+    def round_number(num: float, decimals: int) -> float:
+        """
+        Round a float based on the number of decimals
+        :param num: The number to round
+        :param decimals: The number of decimals
+        :return: The rounded number - int if decimals is 0, float otherwise
+        """
         if isinstance(num, float):
             num = round(num, decimals)
             if decimals == 0:
@@ -25,9 +31,19 @@ class Form1325DFToPDF:
 
     @staticmethod
     def _add_thousands_separator(num: float) -> str:
+        """
+        Get a string representing a number with thousands separator
+        :param num: The number
+        :return: The number as a string with thousands separator
+        """
         return "" if num == "" else f"{num:,}"
 
     def parse_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Parse the DataFrame columns for a good-looking PDF representation
+        :param df: The DataFrame
+        :return: The parsed DataFrame
+        """
         # Round some float column values
         columns_to_round = {
             0: [Heb.BUY_AMOUNT, Heb.BUY_AMOUNT_ADJUSTED, Heb.SELL_AMOUNT, Heb.PROFIT, Heb.LOSS],
@@ -50,22 +66,57 @@ class Form1325DFToPDF:
 
     @staticmethod
     def _tag(tag_name: str, text: str, cls: str = None) -> str:
+        """
+        Create an HTML tag string
+        :param tag_name: The tag name
+        :param text: The text (or HTML) to put inside the tag
+        :param cls: The tag's class name
+        :return: A string with the HTML tag
+        """
         cls_str = '' if cls is None else f' class="{cls}"'
         return fr"<{tag_name}{cls_str}>{text}</{tag_name}>"
 
     def _p_tag(self, text: str, cls: str = None) -> str:
+        """
+        Create an HTML p tag
+        :param text: The text
+        :param cls: The tag's class
+        :return: The HTML p tag string
+        """
         return self._tag("p", text, cls)
 
     def _b_tag(self, text: str, cls: str = None) -> str:
+        """
+        Create an HTML b tag
+        :param text: The text
+        :param cls: The tag's class
+        :return: The HTML b tag string
+        """
         return self._tag("b", text, cls)
 
     def _span_tag(self, text: str, cls: str = None) -> str:
+        """
+        Create an HTML span tag
+        :param text: The text
+        :param cls: The tag's class
+        :return: The HTML span tag string
+        """
         return self._tag("span", text, cls)
 
     def _div_tag(self, text: str, cls: str = None) -> str:
+        """
+        Create an HTML div tag
+        :param text: The text
+        :param cls: The tag's class
+        :return: The HTML div tag string
+        """
         return self._tag("div", text, cls)
 
-    def _get_asset_abroad_html(self):
+    def _get_asset_abroad_html(self) -> str:
+        """
+        Create an HTML string for the asset abroad data
+        :return: The HTML string
+        """
         yes_span = self._span_tag(self._p_tag("X" if self.ASSET_ABROAD else ""), "checkbox")
         no_span = self._span_tag(self._p_tag("X" if not self.ASSET_ABROAD else ""), "checkbox")
         html = self._div_tag(
@@ -75,22 +126,46 @@ class Form1325DFToPDF:
         return html
 
     @staticmethod
-    def _get_html_table(cls: str, data: list[list] = None, df: pd.DataFrame = None, columns: list = None, header=False):
+    def _get_html_table(
+            cls: str, data: list[list] = None, df: pd.DataFrame = None, columns: list = None, header: bool = False
+    ) -> str:
+        """
+        Create an HTML string for a table. Pass either data (with columns) or df (without columns).
+        :param cls: The table's HTML class name
+        :param data: The data
+        :param df: The DataFrame
+        :param columns: The columns (relevant only when passing data)
+        :param header: Should put the header in the html or not
+        :return: The HTML table string
+        """
         if df is None:
             df = pd.DataFrame(data, columns=columns)
         return df.to_html(index=False, escape=False, classes=[cls], header=header)
 
     def _personal_details_table(self) -> str:
+        """
+        Create an HTML table for the personal details table
+        :return: The HTML table string
+        """
         data = [[self.NAME, self.FILE_NUMBER, self._get_asset_abroad_html()]]
         cls = "personal_details_table"
         columns = [Heb.NAME, Heb.FILE_NUMBER, Heb.ASSET_ABROAD]
         return self._get_html_table(cls, data=data, columns=columns, header=True)
 
-    def _data_table(self, df: pd.DataFrame):
+    def _data_table(self, df: pd.DataFrame) -> str:
+        """
+        Create and HTML table for the data table
+        :param df: The DataFrame containing the data
+        :return: The HTML table string
+        """
         cls = "data_table"
         return self._get_html_table(cls, df=df, header=True)
 
     def _total_profit_loss_table(self, total_profit: str, total_loss: str) -> str:
+        """
+        Create an HTML table for the total profit & loss table
+        :return: The HTML table string
+        """
         data = [[
             f"{self._b_tag(Heb.TOTAL_PROFIT_LOSS)}<br/>{self._p_tag(Heb.TOTAL_PROFIT_LOSS_COMMENT)}",
             self._b_tag(total_profit), self._b_tag(total_loss)
@@ -99,6 +174,10 @@ class Form1325DFToPDF:
         return self._get_html_table(cls, data=data)
 
     def _total_sales_table(self, total_sales: str) -> str:
+        """
+        Create an HTML table for the total sales table
+        :return: The HTML table string
+        """
         data = [[
             f"{self._b_tag(Heb.TOTAL_SALES)}<br/>{self._p_tag(Heb.TOTAL_SALES_COMMENT)}",
             self._b_tag(total_sales)
@@ -107,16 +186,31 @@ class Form1325DFToPDF:
         return self._get_html_table(cls, data=data)
 
     def _signatures_table(self) -> str:
+        """
+        Create an HTML table for the signatures table
+        :return: The HTML table string
+        """
         data = [[self._p_tag(Heb.SIGNATURE_1), self._p_tag(Heb.SIGNATURE_2)]]
         cls = "signatures_table"
         return self._get_html_table(cls, data=data)
 
     @staticmethod
     def get_df_column_sum(df: pd.DataFrame, column_name: str) -> int:
+        """
+        Get a total sum of a column from the DataFrame
+        :param df: The DataFrame
+        :param column_name: The columns name
+        :return: An integer with the sum total of the column values
+        """
         result = round(pd.to_numeric(df[column_name], errors="coerce").sum())
         return result
 
     def _df_to_html(self) -> str:
+        """
+        Get the whole document's HTML string
+        :return: The HTML document string
+        """
+        # Calculate some column totals
         total_profit = self._add_thousands_separator(self.get_df_column_sum(self.DF, Heb.PROFIT))
         total_loss = self._add_thousands_separator(self.get_df_column_sum(self.DF, Heb.LOSS))
         total_sales = self._add_thousands_separator(self.get_df_column_sum(self.DF, Heb.SELL_AMOUNT))
@@ -146,6 +240,11 @@ class Form1325DFToPDF:
 
     @staticmethod
     def merge_pdfs(pdf_list, output_path):
+        """
+        Merge multiple PDF files
+        :param pdf_list: A list with paths to PDF files
+        :param output_path: The output path of the merged PDF file
+        """
         pdf_merger = PyPDF2.PdfMerger()
 
         for pdf in pdf_list:
@@ -155,6 +254,10 @@ class Form1325DFToPDF:
             pdf_merger.write(output_file)
 
     def _html_to_pdf(self, html: str):
+        """
+        Write the document's HTML string to a PDF file
+        :param html: The HTML string
+        """
         options = {
             'encoding': 'UTF-8',
             'load-error-handling': 'ignore',
@@ -169,13 +272,19 @@ class Form1325DFToPDF:
             "header-spacing": 6,
             "footer-spacing": 1,
         }
+        # Create the temp PDF file with a uuid as its name
         parent_dir = os.path.dirname(os.path.dirname(__file__))
         temp_pdf = f"{parent_dir}/temp/{uuid.uuid4()}.pdf"
+        # Generate the temp PDF file with a css file for a good-looking output
         if pdfkit.from_string(html, temp_pdf, options=options, css=f"{parent_dir}/resources/form_1325.css"):
+            # Add the explanations PDF page to the temp PDF file
             explanations_pdf = f"{parent_dir}/resources/1325_explanations.pdf"
             self.merge_pdfs([temp_pdf, explanations_pdf], self.OUTPUT_PATH)
-            os.remove(temp_pdf)
+            os.remove(temp_pdf)  # Remove the temp file
 
     def run(self):
+        """
+        Convert the object's DataFrame (self.DF) to HTML, and then from HTML to PDF
+        """
         html = self._df_to_html()
         self._html_to_pdf(html)
